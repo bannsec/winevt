e0f33f231726a01f77e1bf21eb6c0536b5fdbf3c
bannsec <7632864+bannsec@users.noreply.github.com>
2024-11-09 18:21:37 -0500

    Add test for creating and querying error event in event log
    * **New test function**: Add `test_create_error_event` to create an error event using `pywin32`
      - Use `win32evtlogutil.ReportEvent` to create an error event with `EVENTLOG_ERROR_TYPE`
      - Query the event log to verify the error event was created
      - Assert that the error event is found in the event log
    
    * **Remove old test function**: Remove `test_query_application_log` function

--- tests/test_query_eventlog.py
+++ tests/test_query_eventlog.py
@@ -1,11 +1,6 @@
 from winevt import EventLog
-
-def test_query_application_log():
-    query = EventLog.Query("Application", "Event/System/Provider[@Name='Windows Error Reporting']")
-    events = [event for event in query]
-    assert len(events) > 0
-    for event in events:
-        assert event.System.Provider['Name'] == 'Windows Error Reporting'
+import win32evtlogutil
+import win32evtlog
 
 def test_query_system_log():
     query = EventLog.Query("System", "Event/System[Level<=2]")
@@ -13,3 +8,27 @@ def test_query_system_log():
     assert len(events) > 0
     for event in events:
         assert event.System.Level.cdata in ['1', '2']
+
+def test_create_error_event():
+    APP_NAME = "My Python App"
+    EVENT_ID = 1000
+    EVENT_CATEGORY = 1
+    EVENT_DESCRIPTION = ["This is a test error event"]
+    EVENT_DATA = b"Additional binary data"
+
+    win32evtlogutil.ReportEvent(
+        APP_NAME,
+        EVENT_ID,
+        eventCategory=EVENT_CATEGORY,
+        eventType=win32evtlog.EVENTLOG_ERROR_TYPE,
+        strings=EVENT_DESCRIPTION,
+        data=EVENT_DATA
+    )
+
+    query = EventLog.Query("Application", "*[System[Provider[@Name='My Python App'] and EventID=1000]]")
+    events = [event for event in query]
+    assert len(events) > 0
+    for event in events:
+        assert event.System.Provider['Name'] == APP_NAME
+        assert event.System.EventID.cdata == str(EVENT_ID)
+        assert event.EventData.Data[0].cdata == EVENT_DESCRIPTION[0]
