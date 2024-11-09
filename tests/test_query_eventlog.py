20eed5e6c28f98da2f550361b9822fc2215cd228
bannsec <7632864+bannsec@users.noreply.github.com>
2024-11-09 18:40:50 -0500

    Remove debugging statement and event data check in `test_create_error_event` function
    * Remove `print` statement used for debugging
    * Remove check for event data in the last assert statement

--- tests/test_query_eventlog.py
+++ tests/test_query_eventlog.py
@@ -29,8 +29,5 @@ def test_create_error_event():
     events = [event for event in query]
     assert len(events) > 0
     for event in events:
-        print(event)  # Debugging statement
         assert event.System.Provider['Name'] == APP_NAME
         assert event.System.EventID.cdata == str(EVENT_ID)
-        if event.EventData is not None and event.EventData.Data is not None and event.EventData.Data[0].cdata is not None:
-            assert event.EventData.Data[0].cdata == EVENT_DESCRIPTION[0]
